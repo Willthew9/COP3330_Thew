1c887f1051eebb1614997817e27707fc3abcf4fb
Willthew9 <willthew99@gmail.com>
2020-09-14 22:22:11 -0400

    First commit

---
+++ Thew_p1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String []args) {
+        String s = "1234";
+
+        //calls classes and methods
+        Encrypter enc = new Encrypter();
+        String s2 = enc.encrypt(s);
+        Decrypter denc = new Decrypter();
+        String s3 = denc.decrypt(s2);
+
+        //output statement
+        System.out.println("With " + s + " being the original number " + s2 + " is the encrypted value and " + s3 + " is the decrypted value.");
+
+
+    }
+
