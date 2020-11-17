04b9d57484bf4028f08b299e5536f443322a0be7
Willthew9 <70342913+Willthew9@users.noreply.github.com>
2020-11-16 21:06:49 -0500

    Delete Shape3D.java

--- thew_p4/src/Shape3D.java
+++
@@ -1,7 +0,0 @@
-import java.lang.Math;
-import java.util.Scanner;
-public abstract class Shape3D extends Shape{
-
-    //abstract method gets volume of shape
-    public abstract double getVolume();
-}
